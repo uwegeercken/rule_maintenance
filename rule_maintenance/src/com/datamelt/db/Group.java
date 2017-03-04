e8dc43b5766718f1ecdaea1497ba299f737e9d9c
uwegeercken <uwe.geercken@web.de>
2017-03-04 14:50:01 +0100

    changed licence info

--- rule_maintenance/src/com/datamelt/db/Group.java
+++ rule_maintenance/src/com/datamelt/db/Group.java
@@ -1,16 +1,21 @@
 /*
- * 
- * Use of this software and code is only allowed after
- * prior permission by GeeNic Software Solutions Ltd. Copying,
- * redistribution or re-selling of this software is prohibited
- * and protected by national and international laws and/or patents.
- * 
- * All intellectual property rights remain with GeeNic Software SolutonsLtd.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Created on 04.11.2004
- * Author uwe
- */
-
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */ 
 package com.datamelt.db;
 
 import java.sql.PreparedStatement;
